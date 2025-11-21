74cd522901fd419d12d1897c71ef5e77d4cbd630
RIOKOWI <achyarrio2004@gmail.com>
2025-11-21 11:13:39 +0700

    buat stl card

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Home());
 }
 
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dashboard Card',
+      debugShowCheckedModeBanner: false,
+      // home: ,
+    );
+  }
+}
